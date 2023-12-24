4981cdc9041a1d7b91795740e4ad27cacd38b073
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-12-24 11:10:36 +0200

    Module of today 24.12.2023

---
+++ node_vs_js.js
@@ -0,0 +1,9 @@
+// 1. 
+// A. We can use Node.js to execute and run the code outside of the browser 
+// B. The Node.js library's APIs are all asynchronous or non-blocking.
+// C. js applications never buffered any type of data in applications. Node.js applications simply output data in chunks.
+
+// 2. 
+// A. It allows us to build interfaces that react whenever the user hovers the mouse over them.
+// B. It is an open-source, flexible, fast, light-weighted framework.
+// C. It is an interpreted scripting language, and the code can only be executed and run in a web browser
